b4ce6083341f21e91455c5bfaa195cc401d0ef1c
Kevin Williams <61164345+CrankyOldITGuy-Dev@users.noreply.github.com>
2021-07-30 16:50:48 -0700

    Create social.js

---
+++ app/javascript/packs/social.js
@@ -0,0 +1,6 @@
+function socialToggle() {
+  const social = document.getElementById("social");
+  const button = document.getElementById("social-btn");
+  social.classList.toggle('hidden');
+  button.classList.toggle('hidden');
+}
