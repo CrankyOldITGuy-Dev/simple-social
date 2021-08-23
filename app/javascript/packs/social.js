7d9e96683639094a7794b393aad1b9630bb076c2
Kevin Williams <61164345+CrankyOldITGuy-Dev@users.noreply.github.com>
2021-08-23 15:52:34 -0700

    Update social.js
    Added listeners to replace the 'onclick' events in the erb file for webpacker/javascript_pack_tag compatibility.

--- app/javascript/packs/social.js
+++ app/javascript/packs/social.js
@@ -1,6 +1,28 @@
+import $ from 'jquery';
+
 function socialToggle() {
   const social = document.getElementById("social");
   const button = document.getElementById("social-btn");
   social.classList.toggle('hidden');
   button.classList.toggle('hidden');
 }
+
+$(() =>
+    $('button#social-btn').on('click', () => socialToggle())
+);
+
+$(() =>
+    $('button#social-close').on('click', () => socialToggle())
+);
+
+$(() =>
+    $('#fb-btn').on('click', () => socialToggle())
+);
+
+$(() =>
+    $('#twitter-btn').on('click', () => socialToggle())
+);
+
+$(() =>
+    $('#reddit-btn').on('click', () => socialToggle())
+);
